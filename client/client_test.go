a380e780ef32afebde19b49a8df807e76877de9e
fanbingxin <fanbingxin.me@gmail.com>
2015-08-18 17:06:24 +0800

    remove client test

--- client/client_test.go
+++ client/client_test.go
@@ -2,19 +2,6 @@ package raftkv
 
 import "testing"
 
-func TestReadWrite(t *testing.T) {
-	cli := NewClient([]string{"127.0.0.1:8000"}, nil)
-	err := cli.Write([]byte("hello"), []byte("world"))
-	if err != nil {
-		t.Fatal(err)
-	}
-	v, err := cli.Read([]byte("hello"))
-	if err != nil {
-		t.Fatal(err)
-	}
-	t.Logf("%s", v)
-}
-
 func BenchmarkRead(t *testing.B) {
 	cli := NewClient([]string{"127.0.0.1:8000"}, nil)
 	err := cli.Write([]byte("hello"), []byte("world"))
